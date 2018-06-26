abd1b0b489e9b205a4db4cf05d9a094e56cea276
webprofusion-chrisc <christopher.cook@webprofusion.com>
2018-06-26 08:34:51 +0000

    Layout WIP

--- src/components/img-tool/img-tool.tsx
+++ src/components/img-tool/img-tool.tsx
@@ -1,4 +1,4 @@
-import { Component,  State } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 import { ResizeTool } from '../../core/ResizeTool';
 //import { PlatformSpec } from '../../core/Models';
 
@@ -59,69 +59,100 @@ export class ImgTool {
       </ion-header>,
       <ion-content padding>
 
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              1 of 2
+    </ion-col>
+            <ion-col>
+              2 of 2
+    </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              1 of 3
+    </ion-col>
+            <ion-col>
+              2 of 3
+    </ion-col>
+            <ion-col>
+              3 of 3
+    </ion-col>
+          </ion-row>
+        </ion-grid>
         <div>
-          
+
           <div>
             <p class="lead">Tired of creating all your icons and splashscreen sizes manually? You've come to the right place.</p>
             <p>Upload your Icons and/or Splashscreen and we'll do all the hard work to create the sizes you need for each platform:</p>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="panel panel-default">
-                  <div class="panel-body">
-                    <div>
-                      <div>
-                        <h3>1 - Select Your Icon</h3>
-                        <div class="input-group file-upload">
-                          <p>
-                            <input type="file" accept="image/*" id="fileupload-icon" onInput={(event) => this.handleChange('icon', event)} name="FileUploadIcon" />
-                          </p>
+            <ion-grid>
+              <ion-row>
+                <ion-col>
 
-                        </div>
-                        <p>Select an Icon image in PNG format (transparency allowed) measuring 1024x1024 pixels.</p>
+                  <ion-card>
+                    <ion-card-header>
+                      1 - Select Your Icon
+  </ion-card-header>
+                    <ion-card-content>
+                      <div class="input-group file-upload">
+                        <p>
+                          <input type="file" accept="image/*" id="fileupload-icon" onInput={(event) => this.handleChange('icon', event)} name="FileUploadIcon" />
+                        </p>
+
                       </div>
+                      <p>Select an Icon image in PNG format (transparency allowed) measuring at least 1024x1024 pixels.</p>
+                    </ion-card-content>
+                  </ion-card>
 
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                      <h3>2 - Select Your Splashscreen</h3>
+
+                </ion-col>
+                <ion-col>
+                  <ion-card>
+                    <ion-card-header>
+                      1 - Select Your Splashscreen
+  </ion-card-header>
+                    <ion-card-content>
                       <div class="input-group file-upload">
                         <input type="file" accept="image/*" id="fileupload-splash" onInput={(event) => this.handleChange('splash', event)} name="FileUploadSplash" />
                       </div>
-                      <p>Select a Splashscreen image in PNG format (no transparency) measuring 2048x2048 pixels with the
+                      <p>Select a Splashscreen image in PNG format (no transparency) measuring at least 2048x2048 pixels with the
                             important stuff centred, we'll then resize from the centre outwards, sampling the top left corner colour as background.</p>
-                    </div>
-                  </div>
-                </div>
+                    </ion-card-content>
+                  </ion-card>
 
-              </div>
-              <div>
-                <h4>3. Where the magic happens</h4>
+                </ion-col>
+              </ion-row>
+              <ion-row>
+                <ion-col>
+                  <div>
+                    <h4>3. Where the magic happens</h4>
 
-                <p>
-                  Ready to Punch It?
+                    <p>
+                      Ready to Punch It?
  <ion-button type="button" onClick={() => this.process()}>
-                    <i class="fa fa-thumbs-up" aria-hidden="true"></i> Kapow!</ion-button>
-                </p>
+                        <i class="fa fa-thumbs-up" aria-hidden="true"></i> Kapow!</ion-button>
+                    </p>
 
-              </div>
+                  </div>
 
-              <div>
-                {this.progressMessage} {this.percentageProcessed}%
+                  <div>
+                    {this.progressMessage} {this.percentageProcessed}%
                 <div class="progressBar" ></div>
-                </div>
+                  </div>
 
-              {this.isCompleted
-                ?
-                <div>
-                  <ion-button onClick={() => this.downloadZip()}>Download Zip</ion-button>
+                  {this.isCompleted
+                    ?
+                    <div>
+                      <ion-button onClick={() => this.downloadZip()}>Download Zip</ion-button>
 
-                </div>
-                : ""
-              }
-              { /*
+                    </div>
+                    : ""
+                  }
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+
+            { /*
               <div v-if="selectedIconFile || selectedSplashFile">
 
                 <h4>3. Where the magic happens</h4>
@@ -168,13 +199,12 @@ export class ImgTool {
             </div>
             */}
 
-              <div class="well well-sm">
-                <p> We'd really appreciate it if you could tell others about this app:
+            <div class="well well-sm">
+              <p> We'd really appreciate it if you could tell others about this app:
                 <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://apetools.webprofusion.com" data-text="Check out Ape Tools, the quick way to generate all the icons and splash screens for your app:"
-                    data-via="webprofusion">Share</a>
-                </p>
-                <p>If you like this tool you can help by sharing it with others</p>
-              </div>
+                  data-via="webprofusion">Share</a>
+              </p>
+              <p>If you like this tool you can help by sharing it with others</p>
             </div>
           </div>
         </div>
